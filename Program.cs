702307f195328579b451a4db1141153fe6147b44
HoaLe <hoale240803@gmail.com>
2025-05-19 17:48:14 +0700

    Sioux interview question | Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,20 +2,17 @@
 
 // Input: n=5, k=2
 // Output: 9
-Console.WriteLine(Candle.CalculateTotalBurnableCandleV1(5, 2));
-
+Console.WriteLine(Candle.CalculateTotalBurnableCandle(5, 2));
 
 // Input: n=2, k=2
 // Output: 3
 Candle c2 = new Candle();
-Console.WriteLine(Candle.CalculateTotalBurnableCandleV1(2, 2));
-
+Console.WriteLine(Candle.CalculateTotalBurnableCandle(2, 2));
 
 // Input: n=1, k=1
 // Output: infinite loop 
 // Candle c3 = new Candle();
 // Console.WriteLine(Candle.CalculateTotalBurnableCandleV2(1, 1));
-
 
 
 
