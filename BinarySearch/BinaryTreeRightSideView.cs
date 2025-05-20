8c4e8330432881675a878819a1574b78c07a343f
HoaLe <hoale240803@gmail.com>
2025-05-20 15:41:15 +0700

    RightSideView v2

--- BinarySearch/BinaryTreeRightSideView.cs
+++ BinarySearch/BinaryTreeRightSideView.cs
@@ -4,6 +4,30 @@ namespace algorithm.BinarySearch
 {
     public class BinaryTreeRightSideView
     {
+        List<int> res = new List<int>();
+
+        public List<int> RightSideViewV2(TreeNode root)
+        {
+            dfs(root, 0);
+            return res;
+        }
+
+        private void dfs(TreeNode node, int depth)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            if (res.Count == depth)
+            {
+                res.Add(node.val);
+            }
+
+            dfs(node.right, depth + 1);
+            dfs(node.left, depth + 1);
+        }
+
         public List<int> RightSideView(TreeNode root)
         {
             List<int> res = new List<int>();
--- Program.cs
+++ Program.cs
@@ -7,13 +7,13 @@ rootNode.left = new TreeNode(2);
 // rootNode.left.left = new TreeNode(4);
 // rootNode.left.right = new TreeNode(5);
 
-// rootNode.right = new TreeNode(3);
+rootNode.right = new TreeNode(3);
 // rootNode.right.left = new TreeNode(6);
 // rootNode.right.right = new TreeNode(7);
 rootNode.PrintBFS();
 
 BinaryTreeRightSideView b = new BinaryTreeRightSideView();
-var res = b.RightSideView(rootNode);
+var res = b.RightSideViewV2(rootNode);
 rootNode.PrintRightSideView(res);
 
 
