010dfc3fa5643b0d029d6d026e788565278b71f8
HoaLe <hoale240803@gmail.com>
2025-06-12 15:04:37 +0700

    CanJump

---
+++ Greedy/CanJump.cs
@@ -0,0 +1,19 @@
+namespace algorithm.Greedy;
+
+public class CanJumpClass
+{
+    public bool CanJump(int[] nums)
+    {
+        int goal = nums.Length - 1;
+
+        for (int i = nums.Length - 2; i >= 0; i--)
+        {
+            if (i + nums[i] >= goal)
+            {
+                goal = i;
+            }
+        }
+
+        return goal == 0;
+    }
+}
