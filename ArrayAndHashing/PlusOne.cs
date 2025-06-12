4b0f3976727a48073d95ea590d4eda5c343c3c5c
HoaLe <hoale240803@gmail.com>
2025-06-12 12:35:56 +0700

    PlusOne

--- ArrayAndHashing/PlusOne.cs
+++ ArrayAndHashing/PlusOne.cs
@@ -35,4 +35,22 @@ public class PlusOneClass
 
         return res.ToArray();
     }
+
+
+    public int[] PlusOne2(int[] digits)
+    {
+        for (int i = digits.Length - 1; i >= 0; i--)
+        {
+            if (digits[i] < 9)
+            {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        var res = new int[digits.Length + 1];
+        res[0] = 1;
+        return res;
+    }
 }
