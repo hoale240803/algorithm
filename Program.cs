44d4889b150eef7e70ce287da06b2d4df1e07e4d
HoaLe <hoale240803@gmail.com>
2025-07-02 06:51:58 +0700

    LetterCombinationsOfAPhoneNumber

--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
 ﻿
-using algorithm.Backtrackingl;
+using algorithm.Backtracking;
 
-PalindromePartitioning palindromePartitioning = new PalindromePartitioning();
+LetterCombinationsOfAPhoneNumber letterCombinationsOfAPhoneNumber = new LetterCombinationsOfAPhoneNumber();
+var res = letterCombinationsOfAPhoneNumber.LetterCombinations("23");
+var res1 = letterCombinationsOfAPhoneNumber.LetterCombinations1("23");
+
+foreach (var item in res1)
+{
+    Console.WriteLine(item);
+}
+
