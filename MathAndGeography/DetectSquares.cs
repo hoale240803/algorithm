873ca5471645a73c607cb14f0c9ae70ca92c2904
HoaLe <hoale240803@gmail.com>
2025-07-14 00:23:10 +0700

    CountSquares

--- MathAndGeography/DetectSquares.cs
+++ MathAndGeography/DetectSquares.cs
@@ -2,18 +2,49 @@ namespace algorithm.MathAndGeography;
 
 public class CountSquares
 {
+    private Dictionary<(int, int), int> pstCount;
+
+    private List<int[]> pts;
+
     public CountSquares()
     {
-
+        pstCount = new Dictionary<(int, int), int>();
+        pts = new List<int[]>();
     }
 
     public void Add(int[] point)
     {
+        var tuplePoint = (point[0], point[1]);
+        if (!pstCount.ContainsKey(tuplePoint))
+        {
+            pstCount[tuplePoint] = 0;
+        }
 
+        pstCount[tuplePoint]++;
+        pts.Add(point);
     }
 
     public int Count(int[] point)
     {
+        int res = 0;
+        int px = point[0];
+        int py = point[1];
 
+
+
+        foreach (var pt in pts)
+        {
+            int x = pt[0];
+            int y = pt[1];
+            if (Math.Abs(py - y) != Math.Abs(px - x) || x == px || y == py)
+            {
+                continue;
+            }
+
+            res += (pstCount.GetValueOrDefault((x, py))
+            * pstCount.GetValueOrDefault((px, y)));
+
+        }
+        return res;
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
 ﻿using algorithm.MathAndGeography;
 
-Multiplication multiplication = new Multiplication();
-Console.WriteLine(multiplication.Multiply("1", "456")); // Output: "56088"
+
+CountSquares countSquares = new CountSquares();
+countSquares.Add([1, 1]);
+countSquares.Add([2, 2]);
+countSquares.Add([1, 2]);
+
+System.Console.WriteLine(countSquares.Count([2, 1]));   // return 1.
+System.Console.WriteLine(countSquares.Count([3, 3]));
+countSquares.Add([2, 2]);     // Duplicate points are allowed.
+System.Console.WriteLine(countSquares.Count([2, 1]));
