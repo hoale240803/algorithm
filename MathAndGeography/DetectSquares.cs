8374e5ea5f798e6f225000771f95c1a5442307e7
HoaLe <hoale240803@gmail.com>
2025-07-13 23:48:42 +0700

    CountSquares

---
+++ MathAndGeography/DetectSquares.cs
@@ -0,0 +1,19 @@
+namespace algorithm.MathAndGeography;
+
+public class CountSquares
+{
+    public CountSquares()
+    {
+
+    }
+
+    public void Add(int[] point)
+    {
+
+    }
+
+    public int Count(int[] point)
+    {
+
+    }
+}
