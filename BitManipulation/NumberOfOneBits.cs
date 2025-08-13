8fe41b138304cdd753f5b006e6ec039d1b804c52
HoaLe <hoale240803@gmail.com>
2025-08-14 01:08:53 +0700

    NumberOfOneBits

---
+++ BitManipulation/NumberOfOneBits.cs
@@ -0,0 +1,15 @@
+namespace algorithm.BitManipulation;
+
+public class NumberOfOneBits
+{
+    public int HammingWeight(uint n)
+    {
+        int count = 0;
+        while (n != 0)
+        {
+            count += (int)(n & 1);
+            n >>= 1;
+        }
+        return count;
+    }
+}
